5dd4b3e7d704663c93b8333c60f27cc99dd9f376
RahulSingh <chouhanrahul1999@gmail.com>
2026-01-09 01:34:10 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  return (
+    <>
+      <div>hello world</div>
+    </>
+  );
+}
+
+export default App;
